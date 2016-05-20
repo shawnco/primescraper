08f869b9867f834a32aec5d200bf9e1d29b6cf76
Shawnco <shawnc366@gmail.com>
2016-05-20 12:39:57 -0500

    Created install.sql file

---
+++ install.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE primescraper;
+CREATE TABLE primescraper.series
+(
+season int,
+episode int,
+url varchar(100),
+name varchar(100)
+);
+CREATE TABLE primescraper.series_data
+(
+seasons int,
+episodes int
+);
+CREATE TABLE primescraper.sources
+(
+id int NOT NULL AUTO_INCREMENT,
+domain varchar(50),
+preference int,
+type varchar(5)
+);
